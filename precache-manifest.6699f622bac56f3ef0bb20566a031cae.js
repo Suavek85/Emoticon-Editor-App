bd752b3e5440c386569b6694e813f5022cd516cc
SNiemiec <slawekniemiec@gmail.com>
2019-06-09 16:10:57 +0200

    Updates

---
+++ precache-manifest.6699f622bac56f3ef0bb20566a031cae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834a3bd2a5dad1360f5730cd3c2a11f0",
+    "url": "/Emoticon-Editor-App/index.html"
+  },
+  {
+    "revision": "a3e21fc242eb407c6ebf",
+    "url": "/Emoticon-Editor-App/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7ef840d2f95fcaa9cab9",
+    "url": "/Emoticon-Editor-App/static/js/2.d6f35138.chunk.js"
+  },
+  {
+    "revision": "a3e21fc242eb407c6ebf",
+    "url": "/Emoticon-Editor-App/static/js/main.62821d42.chunk.js"
+  },
+  {
+    "revision": "6d047140d26f798d1911",
+    "url": "/Emoticon-Editor-App/static/js/runtime~main.f00f8d32.js"
+  }
+]);
